b61e0841f269b9222357641ec3e20a4c8721b127
linux <aluno@fox.debian>
2019-11-21 19:59:36 -0300

    segundo commit

--- __init__.py
+++ __init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('settings')
 
 from home import views
+
+db = SQLAlchemy(app)
+
+from aluno import views
---
+++ aluno/views.py
@@ -0,0 +1,5 @@
+from yoda2 import app
+
+@app.route('/login'):
+def login():
+    return 'Olá, aluno'
